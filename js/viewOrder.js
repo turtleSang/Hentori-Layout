e5a18826fb742969ab380e875978128873f3a49c
turtle <nguyenthanhsang60309@outlook.com>
2024-01-10 01:08:35 +0700

    update Pagination due Order

--- js/viewOrder.js
+++ js/viewOrder.js
@@ -1,7 +1,7 @@
 
 //function
 //Render Order Info
-const renderOrder = (listOrderDto)=>{
+const renderOrder = (listOrderDto, ElementRender) => {
     let content = "";
     for (const item of listOrderDto) {
         content += `
@@ -27,25 +27,145 @@ const renderOrder = (listOrderDto)=>{
         </div>
         `
     }
-    document.getElementById("list_orders_due").innerHTML = content;
+    ElementRender.querySelector(".list_orders_detail").innerHTML = content;
+}
+
+const renderPageNav = (pageNumber, elementRender) => {
+    let navEle = elementRender.querySelector(".pagination");
+    let content = ``;
+    content += `
+        <li class="page-item" onclick="directionalPagePrevious(event)">
+            <a class="page-link previous" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>    
+    `
+    for (let index = 0; index < pageNumber; index++) {
+        let numPage = index + 1;
+        if (index === 0) {
+            content += `
+            <li class="page-item" onclick="chossePage(event)">
+                <a class="page-link page-num active" href="#">${numPage}</a>
+            </li>            
+            
+            `
+        } else {
+            content += `
+            <li class="page-item" onclick="chossePage(event)">
+                <a class="page-link page-num" href="#">${numPage}</a>
+            </li>
+            
+            `
+        };
+    }
+    content += `
+    <li class="page-item" onclick="directionalPageNext(event)">
+        <a class="page-link next" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+        </a>
+    </li>  
+    `;
+    navEle.innerHTML = content;
 }
 
 //View detail Order
-const viewOrderDetail = (order_id)=>{
-    window.location.href =`./vieworderdetail.html?orderid=${order_id}`;    
+const viewOrderDetail = (order_id) => {
+    window.location.href = `./vieworderdetail.html?orderid=${order_id}`;
 }
 
+// directional
+const directionalPagePrevious = (event) => {
+    event.preventDefault();
+    let navElement = event.currentTarget.parentElement;
+    let listPageNum = navElement.querySelectorAll(".page-num");
+    let indexActive = -1;
+    for (const index in listPageNum) {
+        if (Object.hasOwnProperty.call(listPageNum, index)) {
+            const elementNum = listPageNum[index];
+            if (elementNum.classList.contains("active")) {
+                indexActive = index;
+            }
+        }
+    }
+    if (indexActive != 0) {
+        listPageNum[indexActive].classList.remove("active")
+        indexActive--;
+        listPageNum[indexActive].classList.add("active");
+    }
+    renderNewPagination(navElement);
+}
 
+const directionalPageNext = (event) => {
+    event.preventDefault();
+    let navElement = event.currentTarget.parentElement;
+    let listPageNum = navElement.querySelectorAll(".page-num");
+    let indexActive = -1;
+    for (const index in listPageNum) {
+        if (Object.hasOwnProperty.call(listPageNum, index)) {
+            const elementNum = listPageNum[index];
+            if (elementNum.classList.contains("active")) {
+                indexActive = index;
+            }
+        }
+    }
+    if (indexActive != (listPageNum.length - 1)) {
+        listPageNum[indexActive].classList.remove("active")
+        indexActive++;
+        listPageNum[indexActive].classList.add("active");
+    }
+    renderNewPagination(navElement);    
+}
+
+const chossePage = (event) => {
+    event.preventDefault();
+    let pageChossenEle = event.currentTarget.querySelector(".page-num");
+    let navElement = event.currentTarget.parentElement;
+    let listPageNum = navElement.querySelectorAll(".page-num");
+    for (const item of listPageNum) {
+        item.classList.remove("active");
+    }
+    pageChossenEle.classList.add("active");
+    
+    renderNewPagination(navElement);
+}
 
 //Call API Get due order
+const renderNewPagination = (navElement)=>{
+    let pageChossen = navElement.querySelector(".active");
+    let pageNumber = Number(pageChossen.innerHTML) - 1;
+    let orderAPI = navElement.getAttribute("data-call-api");
+    let idRender = navElement.getAttribute("data-render-id");
+    let elementRender = document.getElementById(idRender);
+    axios({
+        url: `http://localhost:8080/order/${orderAPI}?pageNumber=${pageNumber}`,
+        method: "get"
+    }).then(res => {
+        let { object } = res.data;
+        renderOrder(object, elementRender);
+    }).catch(err => {
+        alert(err)
+    })
+}
 axios({
     url: "http://localhost:8080/order/due",
     method: "GET"
-}).then(res =>{
-    console.log(res);
-    let {object} = res.data;
-    renderOrder(object);
-}).catch(err =>{
+}).then(res => {
+    let { object } = res.data;
+    let elementRender = document.getElementById("order-due-pane");
+    renderOrder(object, elementRender);
+}).catch(err => {
     alert("Không tìm thấy danh sách đến hạn")
 })
+
+axios({
+    url: "http://localhost:8080/order/due/page",
+    method: "GET"
+}).then(res => {
+    let { object } = res.data;
+    let elementRender = document.getElementById("order-due-pane");
+    renderPageNav(object, elementRender);
+}).catch(err => {
+    console.log(err);
+})
+
 
